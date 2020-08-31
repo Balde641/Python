6cc47ec4a736d021941ae968fc7cc38e9502c3d3
Balde641 <62099893+Balde641@users.noreply.github.com>
2020-08-31 09:07:27 +0300

    Update osa2.py

--- osa2.py
+++ osa2.py
@@ -14,25 +14,17 @@ def main():
 
         naytto.fill((0, 0, 0))
         pygame.draw.line(naytto, (0, 0, 255), (0, 0), (640, 400))
-        #pygame.display.flip()
-
-        #naytto.fill((0, 0, 0))
+        
         pygame.draw.line(naytto, (0, 255, 0), (640, 0), (0, 400))
-        #pygame.display.flip()
-
-        #naytto.fill((0, 0, 0))
+        
         pygame.draw.rect(naytto, (255, 0, 0), (100, 50, 150, 200))
-        #pygame.display.flip()
-
-        #naytto.fill((0, 0, 0))
+        
         pygame.draw.circle(naytto, (255, 255, 0), (350, 150), 40)
 
-        #naytto.fill((0, 0, 0))
         kuva = pygame.image.load("mycat.png").convert()
         naytto.blit(kuva, (x, y))
         pygame.display.flip()
-        #pygame.display.flip()
-        # oli osa2
- 
+        
+
 
 main()
