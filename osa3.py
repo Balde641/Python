57cb2e8bc8713e1d9148f6d51662493cbb5f5e73
Balde641 <62099893+Balde641@users.noreply.github.com>
2020-08-31 15:36:59 +0300

    31.8.20

--- osa3.py
+++ osa3.py
@@ -18,17 +18,43 @@ def piirtaminen(naytto, hahmot):
 def kontrolli(hahmot, tapahtuma):
     if tapahtuma.type == pygame.KEYDOWN:
         if tapahtuma.key == pygame.K_SPACE:
+
+            for hahmo in hahmot:
+                hahmo[3] = True
+        elif tapahtuma.key == pygame.K_RIGHT:
+            päähahmo = hahmot[1]
+            if päähahmo[1] < 500:
+                päähahmo[1] += 10
+
+        elif tapahtuma.key == pygame.K_LEFT:
+            päähahmo = hahmot[1]
+            if päähahmo[1] > 10:
+                päähahmo[1] -= 10    
+
+        elif tapahtuma.key == pygame.K_UP:
+            päähahmo = hahmot[1]
+            päähahmo[2] -= 10 
+
+        elif tapahtuma.key == pygame.K_DOWN:
+            päähahmo = hahmot[1]
+            päähahmo[2] += 10        
+
             hahmot[0][3] = True
+            hahmot[1][3] = True
+
 
 def main():
-    kissahahmo = ["mario.jpg", 100, 100, False]
-    hahmot = [kissahahmo]
+    kissahahmo = ["gun.png", 50, 50, False]
+    kissahahmo1 = ["bal1.jpg", 500, 50, False]
+    hahmot = [kissahahmo, kissahahmo1]
+
     while True:
         tapahtuma = pygame.event.poll()
         if tapahtuma.type == pygame.QUIT:
             break
         kontrolli(hahmot, tapahtuma)
         piirtaminen(naytto, hahmot)
+
 
 
 main()
